25aaa1a632fa0c61447038bb289e1c5a1d87329e
prijacash <prijacash@gmail.com>
2022-12-06 15:37:16 -0800

    updated ejs

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+app.set('view engine', 'ejs');
+// app.get('/', (req, res) => {
+//     res.send('Hello, World!');
+// });
+
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname+'/views/index.html');
+//   });
+
+app.get('/', (req, res)=>{
+res.render('index.ejs');
 });
 
+
+
+ 
 app.listen(8000);
