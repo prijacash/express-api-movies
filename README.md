19336e1ecaec0397854e7e5a22696a8d4f1f671e
prijacash <prijacash@gmail.com>
2022-12-06 15:08:19 -0800

    updated initial setup

---
+++ README.md
@@ -0,0 +1,1 @@
+OMDB movie API
---
+++ server.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const express = require('express');
+
+// Sets EJS as the view engine
+
+// Adds some logging to each request
+
+// Routes
+
+// FAVE Routes
+// Get /faves -- READ all faves and dispaly them to user
+
+// POST /faves -- CREATE new face and redirect to /faves to display user faves
